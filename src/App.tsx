740deae490d6f1ae3eb93119fced9325678638ca
Mauricio de Mello <mau-ideias@hotmail.com>
2023-10-03 01:25:12 -0300

    Convertendo os arquivos para styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
+import { ThemeProvider } from 'styled-components'
 import Header from './components/Cabecalho'
 import Hero from './components/Hero'
 import ListaVagas from './containers/ListaVagas'
-
-import './global.css'
+import temaForm from './themes/tema'
+import EstiloGlobal, { Container } from './styles'
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={temaForm}>
+      <EstiloGlobal />
       <Header />
       <Hero />
-      <div className="container">
+      <Container>
         <ListaVagas />
-      </div>
-    </>
+      </Container>
+    </ThemeProvider>
   )
 }
 
---
+++ src/themes/tema.ts
@@ -0,0 +1,11 @@
+const temaForm = {
+  corPrincipal: '#a7727d',
+  corSecundaria: '#f9f5e7'
+}
+
+export default temaForm
+
+export type Theme = {
+  corPrincipal: string
+  corSecundaria: string
+}
